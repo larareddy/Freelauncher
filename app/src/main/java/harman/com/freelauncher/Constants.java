dc1af48bd24f3acc665166fad864feb58558ea49
ADHARMAN\HAlavala <ba.hemanth@tataelxsi.com>
2017-05-02 14:48:11 -0400

    changes made to java code

---
+++ app/src/main/java/harman/com/freelauncher/Constants.java
@@ -0,0 +1,11 @@
+package harman.com.freelauncher;
+
+/**
+ * Created by HAlavala on 5/1/2017.
+ */
+
+class Constants {
+public static final int LEFT_MEAL=1;
+    public static final int RIGHT_MEAL=2;
+
+}
